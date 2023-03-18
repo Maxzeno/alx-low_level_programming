3dc45a415e7f921c1d66c8cefdda732188417417
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-03-18 01:43:39 +0100

    100 and 101 done in if else project

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n1, n2, m1, m2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		
+	}
+	putchar('\n');
+	return (0);
+}
