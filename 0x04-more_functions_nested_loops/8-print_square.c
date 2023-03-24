495abe71ab9f46292c54875332c17828eb72707a
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-03-24 01:47:24 +0100

    in more function am trying to fix number 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,17 @@
 void print_square(int size)
 {
 	int row, column;
-
-	for (row = 1; row <= size; ++row)
+	if (size != 0)
 	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
+		for (row = 1; row <= size; ++row)
+		{
+			for (column = 1; column <= size; ++column)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
