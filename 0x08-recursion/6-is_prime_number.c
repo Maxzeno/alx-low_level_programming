d3a123c3a90d8c4692b881f32119e9e7801749da
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-04-05 22:35:26 +0100

    # 0x08. C - Recursion worked on 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,32 @@
 /**
+ * is_prime_number_wrapper - prime number function
+ * @i : input number
+ * @n : input number
+ *
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+*/
+int is_prime_number_wrapper(int i, int n)
+{
+	int half;
+	half = (n / 2) + 1;
+
+	if (i == half)
+	{
+		return (0);
+	}
+
+	if (n % i == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_number_wrapper(i + 1, n));
+	}
+}
+
+
+/**
  * is_prime_number - prime number function
  * @n : input number
  *
@@ -6,16 +34,6 @@
 */
 int is_prime_number(int n)
 {
-	int i, half;
-	half = (n / 2) + 1;
-
-	for (i = 2; i <= half; i++)
-	{
-		if (n % i == 0)
-		{
-			return (1);
-		}
-	}
-	return (0);
+	return (is_prime_number_wrapper(2, n));
 
 }
