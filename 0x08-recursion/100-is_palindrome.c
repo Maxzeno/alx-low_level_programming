28a03ea676f874668f2fea33039a3e8d3d3485db
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-04-05 23:31:48 +0100

    # 0x08. C - Recursion worked on 7 and 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,15 +12,15 @@ int is_palindrome(char *s);
  */
 int find_strlen(char *s)
 {
-    int len = 0;
+int len = 0;
 
-    if (*(s + len))
-    {
-        len++;
-        len += find_strlen(s + len);
-    }
+if (*(s + len))
+{
+    len++;
+    len += find_strlen(s + len);
+}
 
-    return (len);
+return (len);
 }
 
 /**
@@ -34,13 +34,13 @@ int find_strlen(char *s)
  */
 int check_palindrome(char *s, int len, int index)
 {
-    if (s[index] == s[len / 2])
-        return (1);
+if (s[index] == s[len / 2])
+    return (1);
 
-    if (s[index] == s[len - index - 1])
-        return (check_palindrome(s, len, index + 1));
+if (s[index] == s[len - index - 1])
+    return (check_palindrome(s, len, index + 1));
 
-    return (0);
+return (0);
 }
 
 /**
@@ -52,11 +52,11 @@ int check_palindrome(char *s, int len, int index)
  */
 int is_palindrome(char *s)
 {
-    int index = 0;
-    int len = find_strlen(s);
+int index = 0;
+int len = find_strlen(s);
 
-    if (!(*s))
-        return (1);
+if (!(*s))
+    return (1);
 
-    return (check_palindrome(s, len, index));
+return (check_palindrome(s, len, index));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,18 +14,18 @@ int wildcmp(char *s1, char *s2);
  */
 int strlen_no_wilds(char *str)
 {
-    int len = 0, index = 0;
+int len = 0, index = 0;
 
-    if (*(str + index))
-    {
-        if (*str != '*')
-            len++;
+if (*(str + index))
+{
+    if (*str != '*')
+        len++;
 
-        index++;
-        len += strlen_no_wilds(str + index);
-    }
+    index++;
+    len += strlen_no_wilds(str + index);
+}
 
-    return (len);
+return (len);
 }
 
 /**
@@ -35,11 +35,11 @@ int strlen_no_wilds(char *str)
  */
 void iterate_wild(char **wildstr)
 {
-    if (**wildstr == '*')
-    {
-        (*wildstr)++;
-        iterate_wild(wildstr);
-    }
+if (**wildstr == '*')
+{
+    (*wildstr)++;
+    iterate_wild(wildstr);
+}
 }
 
 /**
@@ -54,19 +54,19 @@ void iterate_wild(char **wildstr)
  */
 char *postfix_match(char *str, char *postfix)
 {
-    int str_len = strlen_no_wilds(str) - 1;
-    int postfix_len = strlen_no_wilds(postfix) - 1;
+int str_len = strlen_no_wilds(str) - 1;
+int postfix_len = strlen_no_wilds(postfix) - 1;
 
-    if (*postfix == '*')
-        iterate_wild(&postfix);
+if (*postfix == '*')
+    iterate_wild(&postfix);
 
-    if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
-    {
-        postfix++;
-        return (postfix_match(str, postfix));
-    }
+if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+{
+    postfix++;
+    return (postfix_match(str, postfix));
+}
 
-    return (postfix);
+return (postfix);
 }
 
 /**
@@ -79,17 +79,17 @@ char *postfix_match(char *str, char *postfix)
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        iterate_wild(&s2);
-        s2 = postfix_match(s1, s2);
-    }
+if (*s2 == '*')
+{
+    iterate_wild(&s2);
+    s2 = postfix_match(s1, s2);
+}
 
-    if (*s2 == '\0')
-        return (1);
+if (*s2 == '\0')
+    return (1);
 
-    if (*s1 != *s2)
-        return (0);
+if (*s1 != *s2)
+    return (0);
 
-    return (wildcmp(++s1, ++s2));
+return (wildcmp(++s1, ++s2));
 }
