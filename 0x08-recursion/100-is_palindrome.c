36c8a35a8687057587aa65e7b5e7d04d161da687
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-04-05 23:19:06 +0100

    # 0x08. C - Recursion worked on 7 and 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,29 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: input string
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+        return (0);
+    return (_strlen_recursion(s + 1) + 1);
+}
+
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
- *
+ * @s: input string
  * Return: 1 if s is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-    int len = 0;
-    int i;
-
-    /* Find the length of the string */
-    if (s[len] != '\0')
-    {
-        len++;
-        return is_palindrome(*s)
-    }
-
-    /* Check if the string is empty or only one character long */
+    int len = _strlen_recursion(s);
     if (len <= 1)
-    {
-        return 1;
-    }
-
-    /* Check if the first and last characters are the same */
-    if (s[0] == s[len-1])
-    {
-        /* Call the function recursively with the substring between the first and last characters */
-        return is_palindrome(s+1) && is_palindrome(s+(len-1)-1);
-    }
-    else
-    {
-        return 0;
-    }
+        return (1);
+    if (*s != *(s + len - 1))
+        return (0);
+    *(s + len - 1) = '\0';
+    return (is_palindrome(s + 1));
 }
