52ec8073d635502886953c54839becca9e6183d3
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-04-05 22:04:35 +0100

    # 0x08. C - Recursion worked on 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,21 +7,19 @@
  *
  * Return: square root of @n or -1
 */
-int _sqrt_recursion_wrapper(int n, int min, int max)
-{
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
-		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+int _sqrt_recursion_wrapper(int guess, int n)
+{	
+	if (guess*guess != n)
+	{
+		if (guess*guess < n)
+			_sqrt_recursion_wrapper(guess+1, n);
+		else
+			return -1;
+	}
+	else
+	{
+		return guess;
+	}
 }
 
 /**
@@ -41,5 +39,5 @@ int _sqrt_recursion(int n)
 	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
 		return (-1);
 	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+		return (_sqrt_recursion_wrapper(1, n));
 }
