f6321192800bd7b09c7e1962ebf5e20ea04adb15
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-04-05 22:42:25 +0100

    # 0x08. C - Recursion worked on 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,10 +7,24 @@
 */
 int is_prime_number_wrapper(int i, int n)
 {
+	if (n < 0)
+	{
+		n = n * -1;
+	}
 	int half;
+
 	half = (n / 2) + 1;
 
-	if (i == half)
+	if (n == 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (0);
+	}
+
+	if (i >= half)
 	{
 		return (0);
 	}
@@ -35,5 +49,4 @@ int is_prime_number_wrapper(int i, int n)
 int is_prime_number(int n)
 {
 	return (is_prime_number_wrapper(2, n));
-
 }
