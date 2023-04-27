88dba4b5fb1447a38f2488b13198c9bb03fba003
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-04-27 21:06:23 +0100

    singly linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,29 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
- * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
+ * list_len - gets the number of nodes
  *
- * Return: The number of elements in h.
- */
+ * @h: pointer to the head (first node)
+ *
+ * Return: number of nodes
+*/
+
 size_t list_len(const list_t *h)
 {
-	size_t elements;
+	size_t node_count = 1;
 
-	elements = 0;
+	/*if head is null return 0 nodes*/
+	if (h == NULL)
+		return (0);
 
-	while (h)
+	/*iterate through the nodes*/
+	while (h->next != NULL)
 	{
-		elements = elements + 1;
+		/*go to the next node*/
 		h = h->next;
+		/*count node*/
+		node_count++;
 	}
 
-	return (elements);
+	return (node_count);
 }
