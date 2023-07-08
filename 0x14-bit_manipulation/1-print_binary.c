0e2bae64f61a3f982fe1959e16017716131e16a7
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-07-08 02:26:52 +0100

    added 1-print_binary.c modify

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,13 @@
 #include "main.h"
 
 /**
- * print_binary - returns the binary of a number
- * @n: number
- * Return: nothing
+ * print_binary - Outputs binary representation.
+ * @n: number.
  */
 void print_binary(unsigned long int n)
 {
-    unsigned long int track, result, base, power;
-    unsigned int i;
-    char remain;
+    if (n > 1)
+        print_binary(n >> 1);
 
-    remain = 0;
-    base = 2;
-    power = sizeof(unsigned long int) * 8 - 1;
-    track = 1;
-    for (i = 1; i <= power; i++)
-        track *= base;
-
-    while (track != 0)
-    {
-        result = n & track;
-        if (result == track)
-        {
-            remain = 1;
-            _putchar('1');
-        }
-        else if (remain == 1 || track == 1)
-        {
-            _putchar('0');
-        }
-        track >>= 1;
-    }
+    _putchar((n & 1) + '0');
 }
