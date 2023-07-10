9658ba1c49de8a901935f2b1f90a8cc96437a3b2
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-07-10 15:43:19 +0100

    updated2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	openfile = open(filename, O_WRONLY | O_APPEND);
-	writefile = writefile(openfile, text_content, length);
+	writefile = write(openfile, text_content, length);
 
 	if (openfile == -1 || writefile == -1)
 		return (-1);
