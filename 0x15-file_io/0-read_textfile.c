285fa6f06a9a7ea300db2ab16ef53452beaa0aa7
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-07-10 15:24:34 +0100

    added 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads text file
+ * @filename: Name of file.
+ * @letters: Number of letters to read.
+ *
+ * Return: Returns the file text.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffMemory;
+	ssize_t openFile, readFile, writeFile;
+
+	buffMemory = malloc(sizeof(char) * letters);
+
+	if (filename == NULL || buffMemory == NULL)
+		return (0);
+
+	openFile = open(filename, O_RDONLY);
+	readFile = read(openFile, buffMemory, letters);
+	writeFile = write(STDOUT_FILENO, buffMemory, readFile);
+
+	if (openFile == -1 || readFile == -1 || writeFile == -1 || writeFile != readFile)
+	{
+		free(buffMemory);
+		return (0);
+	}
+
+	free(buffMemory);
+	close(openFile);
+
+	return (writeFile);
+}
