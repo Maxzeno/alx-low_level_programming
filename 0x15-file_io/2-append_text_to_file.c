2e589c3fc686648108f20c1926285367c4211d03
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-07-10 15:41:38 +0100

    added 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text to file.
+ * @filename: Name of file
+ * @text_content: Content
+ *
+ * Return: 1 or -1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int openfile, writefile, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	openfile = open(filename, O_WRONLY | O_APPEND);
+	writefile = writefile(openfile, text_content, length);
+
+	if (openfile == -1 || writefile == -1)
+		return (-1);
+
+	close(openfile);
+
+	return (1);
+}
