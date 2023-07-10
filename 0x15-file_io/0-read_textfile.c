e370a45e4df8a20b7ad2ae3d2aa94b0b36cd5204
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-07-10 15:33:55 +0100

    modified 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,28 +10,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
-	ssize_t openfile, readfile, writefile;
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	openfile = open(filename, O_RDONLY);
-	readfile = read(openfile, buff, letters);
-	writefile = write(STDOUT_FILENO, buff, readfile);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (openfile == -1 || readfile == -1 || writefile == -1 || writefile != readfile)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buff);
+		free(buffer);
 		return (0);
 	}
 
-	free(buff);
-	close(openfile);
+	free(buffer);
+	close(o);
 
-	return (writefile);
+	return (w);
 }
