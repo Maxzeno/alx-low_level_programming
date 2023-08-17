a9677647e67cd737d27e8534bce141f56b966e5d
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-08-17 19:28:20 +0100

    added 4-free_dlistint.c 5-get_dnodeint.c 6-sum_dlistint.c 7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_dnodeint_at_index - Locates a node in a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ * @index: The node to index.
+ *
+ * Return: null or address of the located node if exists.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+
+	return (head);
+}
